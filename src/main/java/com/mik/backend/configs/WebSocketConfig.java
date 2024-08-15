fe9e79d315e268bf80677fe6ef93c83044c27804
Bulat <bulatsafiullin2612@gmail.com>
2024-08-15 15:07:38 +0300

    edit WebSocketConfig

--- src/main/java/com/mik/backend/configs/WebSocketConfig.java
+++ src/main/java/com/mik/backend/configs/WebSocketConfig.java
@@ -28,7 +28,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:5173");
+                .setAllowedOrigins("http://localhost:5173")
+                .withSockJS();
     }
 
     @Override
